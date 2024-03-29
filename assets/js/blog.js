ccdfb57e38f131ecf221f705971d22c30a5e85aa
aidan BL <aidanjohn13@gmail.com>
2024-03-29 00:31:16 -0400

    third commit

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -1,21 +1,21 @@
-// oo wowee
+// oo wowee this one adds all exsisting posts to the main element with class blogPosts
 window.onload = function() {
     const blogPosts = document.getElementById("blogPosts");
     const posts = JSON.parse(localStorage.getItem("posts")) || [];
-
+    //creates a separate div element for each post object
     posts.forEach(function(post) {
         const postDiv = document.createElement('div');
         postDiv.classList.add('post');
-
-        const usernamePara = document.createElement('p');
+        //makes the content of username into a paragraph 
+        const usernamePara = document.createElement('h2');
         usernamePara.textContent = "Username: " + post.username;
-
-        const titlePara = document.createElement('p');
+        //makes the content of title into a paragraph 
+        const titlePara = document.createElement('h3');
         titlePara.textContent = "Title: " + post.title;
-
+        //makes the content of 'Content' into a paragraph
         const contentPara = document.createElement('p');
         contentPara.textContent = "Content: " + post.content;
-
+        //adds all changes
         postDiv.appendChild(usernamePara);
         postDiv.appendChild(titlePara);
         postDiv.appendChild(contentPara);
