99d029c0b16e8ab661190477c7fe4d76b33e0718
aidan BL <aidanjohn13@gmail.com>
2024-03-28 20:57:35 -0400

    initial commit, very buggy

---
+++ assets/js/blog.js
@@ -0,0 +1,24 @@
+window.onload = function() {
+    const blogPosts = document.getElementById("blogPosts");
+    const posts = JSON.parse(localStorage.getItem("posts")) || [];
+
+    posts.forEach(function(post) {
+        const postDiv = document.createElement('div');
+        postDiv.classList.add('post');
+
+        const usernamePara = document.createElement('p');
+        usernamePara.textContent = "Username: " + post.username;
+
+        const titlePara = document.createElement('p');
+        titlePara.textContent = "Title: " + post.title;
+
+        const contentPara = document.createElement('p');
+        contentPara.textContent = "Content: " + post.content;
+
+        postDiv.appendChild(usernamePara);
+        postDiv.appendChild(titlePara);
+        postDiv.appendChild(contentPara);
+
+        blogPosts.appendChild(postDiv);
+    });
+};
